421f28257f72e88c201e700c13efb2ca66515a8d
Steve <sjpearce@gmail.com>
2020-04-15 10:05:43 +0100

    Changes.

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,6 +19,7 @@ from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,3 +39,4 @@ urlpatterns += [
 # Use static() to add url mapping to serve static files during development (only)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
